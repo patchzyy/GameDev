5c2e44f2af87fafda09e6eed04f81c9302f7bdbe
max geurtsen <mgeurtsen@moreapp.com>
2026-05-21 10:28:55 +0200

    Friendlies no longer target recently healed zombies

--- TheCure/Entities/Friendlies/Friendly.cs
+++ TheCure/Entities/Friendlies/Friendly.cs
@@ -336,7 +336,7 @@ namespace TheCure
             {
                 if (enemy == null) continue;
 
-                if (enemy is Zombie zombie && zombie.LastHealed < 3f)
+                if (enemy is Zombie zombie && zombie.LastHealed < 7f)
                     continue;
 
                 float dist = Vector2.Distance(((CircleCollider)enemy.collider).Center, ((CircleCollider)collider).Center);
